98f2b37a7f7cdd314057b9dc8e5c098478e540f2
Arthur <arthurdai97@gmail.com>
2025-06-16 21:12:12 +0100

    fix left sidebar tabs

--- src/components/projects-nav/ProjectsNav.jsx
+++ src/components/projects-nav/ProjectsNav.jsx
@@ -4,13 +4,21 @@ const ProjectsNav = () => {
   return (
     <nav className="bg-[#393E46] h-[calc(100vh-72px)] text-white flex flex-col w-3xs text-base p-6 overflow-y-auto">
       <ul className="flex flex-col gap-3">
-        <li className="flex justify-center bg-white text-black p-2 rounded-md">
-          <Link to={"/dashboard/projects/create-project"}>
+        <li className="flex bg-white text-black rounded-md">
+          <Link
+            className="flex justify-center w-full p-2"
+            to={"/dashboard/projects/create-project"}
+          >
             Create Project +
           </Link>
         </li>
-        <li className="flex justify-center bg-white text-black p-2 rounded-md">
-          <Link to={"/dashboard/projects/personal-projects"}>Projects</Link>
+        <li className="flex bg-white text-black rounded-md">
+          <Link
+            className="flex justify-center w-full p-2"
+            to={"/dashboard/projects/personal-projects"}
+          >
+            Projects
+          </Link>
         </li>
       </ul>
     </nav>
