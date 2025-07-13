eaa4388b6ebdbcbe65df1cc077a9123483fbe0ce
Arthur <arthurdai97@gmail.com>
2025-07-13 20:13:22 +0100

    add list of projects page

--- src/pages/dashboard/projects-ui/MoreProject.jsx
+++ src/pages/dashboard/projects-ui/MoreProject.jsx
@@ -1,5 +1,32 @@
+import { useFormDataContext } from "../../../contexts";
+import { Link } from "react-router-dom";
+
 const MoreProject = () => {
-  return <h1>My Name is AD</h1>;
+  const { formData } = useFormDataContext();
+
+  return (
+    <div className="w-[calc(1200px-256px)] overflow-y-auto border-r-3 border-[#393E46]">
+      <div className="flex flex-col gap-10 p-15  h-[calc(100vh-80px)]">
+        <h1 className="text-3xl">List of Projects</h1>
+
+        <div className="flex flex-wrap gap-5">
+          {formData.map((data) => {
+            return (
+              <Link
+                to={`/dashboard/projects/${data.id}`}
+                className="flex flex-col gap-3 border-2 border-solid border-black rounded-md p-5"
+                key={data.id}
+              >
+                <h1 className="text-4xl text-center">{data.project_name}</h1>
+                <p>Author: {data.author}</p>
+                <p>Starting Date: {data.starting_date}</p>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MoreProject;
