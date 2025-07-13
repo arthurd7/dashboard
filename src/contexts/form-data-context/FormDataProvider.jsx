611d6289e5df294180a7b56b1655aa99497335e9
Arthur <arthurdai97@gmail.com>
2025-07-13 20:14:58 +0100

    improve formDataProvider with localStorage

--- src/contexts/form-data-context/FormDataProvider.jsx
+++ src/contexts/form-data-context/FormDataProvider.jsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FormDataContext } from "./FormDataContext";
 
 export const FormDataProvider = ({ children }) => {
-  const [formData, setFormData] = useState([]);
+  // preset the initial value of formData, getting the saved data from localStorage
+  const [formData, setFormData] = useState(() => {
+    const savedFormData = localStorage.getItem("formData");
+    return savedFormData ? JSON.parse(savedFormData) : [];
+  });
+
+  // save the latest formData, every time formData changes (including inital render)
+  useEffect(() => {
+    localStorage.setItem("formData", JSON.stringify(formData));
+  }, [formData]);
 
   return (
     <FormDataContext.Provider value={{ formData, setFormData }}>
