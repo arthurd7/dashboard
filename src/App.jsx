aacced052e997e256e5a27f8817b9f8328c06758
Arthur <arthurdai97@gmail.com>
2025-07-20 00:47:58 +0100

    change to createHashRouter for Github Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { DashboardLayout, Introduction, ProjectsLayout } from "./pages";
 import {
   Instruction,
@@ -7,43 +7,38 @@ import {
   ProjectDocument,
 } from "./pages/dashboard";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <DashboardLayout />,
-      // these are all tabs the tabs content
-      children: [
-        { index: true, element: <Introduction /> },
-        {
-          path: "projects",
-          element: <ProjectsLayout />,
-          children: [
-            {
-              index: true,
-              element: <Instruction />,
-            },
-            {
-              path: "create-project",
-              element: <CreateProject />,
-            },
-            {
-              path: "personal-projects",
-              element: <MoreProject />,
-            },
-            {
-              path: ":projectId",
-              element: <ProjectDocument />,
-            },
-          ],
-        },
-      ],
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: "/dashboard",
-  }
-);
+    path: "/",
+    element: <DashboardLayout />,
+    // these are all tabs the tabs content
+    children: [
+      { index: true, element: <Introduction /> },
+      {
+        path: "projects",
+        element: <ProjectsLayout />,
+        children: [
+          {
+            index: true,
+            element: <Instruction />,
+          },
+          {
+            path: "create-project",
+            element: <CreateProject />,
+          },
+          {
+            path: "personal-projects",
+            element: <MoreProject />,
+          },
+          {
+            path: ":projectId",
+            element: <ProjectDocument />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/dashboard/",
+  base: "./",
   // resolve: {
   //   alias: {
   //     "@": path.resolve(__dirname, "src"),
