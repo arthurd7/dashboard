a25ba59af01b4d47127c6f40de6d560c396fb9bf
Arthur <arthurdai97@gmail.com>
2025-07-20 00:11:25 +0100

    adjust basename in router

--- src/App.jsx
+++ src/App.jsx
@@ -7,41 +7,43 @@ import {
   ProjectDocument,
 } from "./pages/dashboard";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <DashboardLayout />,
-    // these are all tabs the tabs content
-    children: [
-      { index: true, element: <Introduction /> },
-      {
-        path: "projects",
-        element: <ProjectsLayout />,
-        children: [
-          {
-            index: true,
-            element: <Instruction />,
-          },
-          {
-            path: "create-project",
-            element: <CreateProject />,
-          },
-          {
-            path: "personal-projects",
-            element: <MoreProject />,
-          },
-          {
-            path: ":projectId",
-            element: <ProjectDocument />,
-          },
-        ],
-      },
-    ],
-  },
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <DashboardLayout />,
+      // these are all tabs the tabs content
+      children: [
+        { index: true, element: <Introduction /> },
+        {
+          path: "projects",
+          element: <ProjectsLayout />,
+          children: [
+            {
+              index: true,
+              element: <Instruction />,
+            },
+            {
+              path: "create-project",
+              element: <CreateProject />,
+            },
+            {
+              path: "personal-projects",
+              element: <MoreProject />,
+            },
+            {
+              path: ":projectId",
+              element: <ProjectDocument />,
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
     basename: "/dashboard",
-  },
-]);
+  }
+);
 
 function App() {
   return <RouterProvider router={router} />;
