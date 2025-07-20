9ae4e11649ffc6c8682176d6e606f7e7f4a9c38e
Arthur <arthurdai97@gmail.com>
2025-07-20 01:19:47 +0100

    update vite config setting

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./",
+  base: "/dashboard/",
   // resolve: {
   //   alias: {
   //     "@": path.resolve(__dirname, "src"),
